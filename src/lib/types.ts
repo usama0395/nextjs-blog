39a04e6fa530942404f1540d63fec20b9df8f0f7
Ubaid Malik <ubaidmalik0303@gmail.com>
2025-10-08 01:50:14 +0500

    ts error resolv again

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,11 +1,10 @@
-import { Image } from "sanity"
 
 export interface BlogsData {
   title:string  
   smallDescription:string  
   currentSlug:string  
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  titleImage:Image
+  titleImage:any
 }
 
 export interface SingleBlog extends Omit<BlogsData, "smallDescription"> {
