06df30e6a83fabc62497806904df225727e70c03
Ubaid Malik <ubaidmalik0303@gmail.com>
2025-10-08 01:46:44 +0500

    ts error resolv again

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -4,6 +4,7 @@ export interface BlogsData {
   title:string  
   smallDescription:string  
   currentSlug:string  
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   titleImage:Image
 }
 
