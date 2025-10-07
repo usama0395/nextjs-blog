04bd52a6189984f780482e51f680e85c291b1829
Ubaid Malik <ubaidmalik0303@gmail.com>
2025-10-08 01:53:22 +0500

    ts error resolv 3

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -3,8 +3,13 @@ export interface BlogsData {
   title:string  
   smallDescription:string  
   currentSlug:string  
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  titleImage:any
+  titleImage: {
+    _type: string;
+    asset: {
+      _ref: string;
+      _type: string;
+    };
+  };
 }
 
 export interface SingleBlog extends Omit<BlogsData, "smallDescription"> {
